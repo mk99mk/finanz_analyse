bfcb7ddd04da2196e2e78e2944bb2a14c7620dc9
Michel Korfhage <korfhage.michel@web.de>
2024-05-19 09:51:14 +0200

    init load from yfinance

---
+++ finanz_analyse/data_loader/finance_yahoo_adapter.py
@@ -0,0 +1,33 @@
+import yfinance as yf
+import pandas as pd
+
+from pathlib import Path
+
+from requests import Session
+from requests_cache import CacheMixin, SQLiteCache
+from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
+from pyrate_limiter import Duration, RequestRate, Limiter
+
+ASSETS = Path(__file__).parent.parent / "assets"
+
+class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
+    pass
+
+session = CachedLimiterSession(
+    limiter=Limiter(RequestRate(2, Duration.SECOND*5)),  # max 2 requests per 5 seconds
+    bucket_class=MemoryQueueBucket,
+    backend=SQLiteCache("yfinance.cache"),
+)
+
+hitories = {}
+
+def load_tickers(symbols: list[str]):
+    global session
+    tickers = yf.Tickers(symbols, session=session)
+    print(tickers.tickers)
+    for symbol, ticker in tickers.tickers.items():
+        history = ticker.history(start='2015-01-01', interval='1d', repair=True)
+        hitories[symbol] = history
+
+def main():
+    load_tickers(["AAPL", "EXW1.DE"])

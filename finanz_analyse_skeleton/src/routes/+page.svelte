ab04d0e9a81167917c41b492bd9917816c21aaac
Michel Korfhage <korfhage.michel@web.de>
2024-07-26 12:27:12 +0200

    init skeleton frontend

---
+++ finanz_analyse_skeleton/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Welcome back.</h2>
+		<div class="flex justify-center space-x-2">
+			<a
+				class="btn variant-filled"
+				href="https://skeleton.dev/"
+				target="_blank"
+				rel="noreferrer"
+			>
+				Launch Documentation
+			</a>
+		</div>
+		<div class="space-y-2">
+			<p>Try editing the following:</p>
+			<p><code class="code">/src/routes/+layout.svelte</code></p>
+			<p><code class="code">/src/routes/+page.svelte</code></p>
+		</div>
+	</div>
+</div>
---
+++ finanz_analyse_skeleton/src/routes/stocks/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">See your stocks here</h2>
+		<div class="flex justify-center space-x-2">
+			<iframe width="1300px" height="700px" src="/iris.html" title="description"></iframe>
+		</div>
+	</div>
+</div>
